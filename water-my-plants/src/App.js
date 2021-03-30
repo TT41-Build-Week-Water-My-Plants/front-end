de132a5fdac19342b50ddeabf99f57b3c3ef4bcb
David <davidellis1020@gmail.com>
2021-03-30 15:38:51 -0700

    Created plantlist and addplants file, PrivateRoute feature

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,3 +1,9 @@
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+//import Login from './components/Login';
+import PrivateRoute from "./components/PrivateRoute";
+import PlantsList from "./components/PlantsList";
+import AddPlantForm from "./components/AddPlantForm";
+
 function App() {
   return <div></div>;
 }
